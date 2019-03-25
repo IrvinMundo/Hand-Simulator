5fa1eda957baee2e118b92690c51f14ae2d1f333
irvin <irvinumundo@hotmail.com>
2019-03-24 18:52:56 -0600

    Card weights added

--- Card.java
+++ Card.java
@@ -1,17 +1,38 @@
 public class Card {
 
     String name;
-    int copies;
-    public Card (String name) {
+    //4 for indispensable, 3 for preferable, 2 for considerable, 1 por possible brick, 0 for brick
+    int desirable;
+    // 1 for not once per turn, 0 for once per turn and -1 for once per duel
+    int restriction;
+
+    public Card (String name, int desirable, int restriction) {
         this.name = name;
+        this.desirable = desirable;
+        this.restriction = restriction;
     }
 
-    public String getName() {
+    public String getName () {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName (String name) {
         this.name = name;
     }
 
+    public int getDesirable () {
+        return desirable;
+    }
+
+    public void setDesirable (int desirable) {
+        this.desirable = desirable;
+    }
+
+    public int getRestriction () {
+        return restriction;
+    }
+
+    public void setRestriction (int restriction) {
+        this.restriction = restriction;
+    }
 }
